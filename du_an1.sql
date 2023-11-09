1fd27aee514af142a110b3397db7bbae03592cce
Edd <55090875+Corey-Stowe@users.noreply.github.com>
2023-11-09 10:46:15 +0700

    Thêm cột lượt xem vào bảng sản phẩm
    #1 Test đổ dữ liệu cắt giao diện admin
    #2 chọn lọc trang khách hàng của ông Vũ Anh chưa thấy submit
    #3 file SQL sẽ lên cả 2 bên jira và bên github
    #4 Bên khách hàng và admin đã tách CSS và JS nên chỉ cẩn chọn lọc giao diện

--- du_an1.sql
+++ du_an1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2023 at 04:41 AM
+-- Generation Time: Nov 09, 2023 at 04:45 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,129 @@ SET time_zone = "+00:00";
 --
 -- Database: `du_an1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner_anh`
+--
+
+CREATE TABLE `banner_anh` (
+  `id` int(11) NOT NULL,
+  `anh` varchar(200) DEFAULT NULL,
+  `anh1` varchar(200) DEFAULT NULL,
+  `anh2` varchar(200) DEFAULT NULL,
+  `anh3` varchar(200) DEFAULT NULL,
+  `anh4` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `binh_luan`
+--
+
+CREATE TABLE `binh_luan` (
+  `ma_binh_luan` int(11) NOT NULL,
+  `ma_khach_hang` int(11) DEFAULT NULL,
+  `ma_san_pham` int(11) DEFAULT NULL,
+  `noi_dung` varchar(200) DEFAULT NULL,
+  `anhbl` varchar(200) DEFAULT NULL,
+  `ngay_binh_luan` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chi_tiet_don_hang`
+--
+
+CREATE TABLE `chi_tiet_don_hang` (
+  `ma_chi_tiet_don_hang` int(11) NOT NULL,
+  `ma_don_hang` int(11) DEFAULT NULL,
+  `ma_san_pham` int(11) DEFAULT NULL,
+  `so_luong` int(11) DEFAULT NULL,
+  `don_gia` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `don_hang`
+--
+
+CREATE TABLE `don_hang` (
+  `ma_don_hang` int(11) NOT NULL,
+  `ngay_dat_hang` date DEFAULT NULL,
+  `ma_khach_hang` int(11) DEFAULT NULL,
+  `trang_thai_don` tinyint(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `khach_hang`
+--
+
+CREATE TABLE `khach_hang` (
+  `ma_khach_hang` int(11) NOT NULL,
+  `ten_khach_hang` varchar(200) DEFAULT NULL,
+  `dia_chi` varchar(200) DEFAULT NULL,
+  `sdt` varchar(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mat_khau` varchar(200) DEFAULT NULL,
+  `avt` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loai_hang`
+--
+
+CREATE TABLE `loai_hang` (
+  `ma_loai` int(11) NOT NULL,
+  `ten_loai` varchar(50) NOT NULL,
+  `trang_thai` tinyint(2) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `loai_hang`
+--
+
+INSERT INTO `loai_hang` (`ma_loai`, `ten_loai`, `trang_thai`) VALUES
+(1, 'Giầy', 0),
+(2, 'Dép', 0),
+(3, 'Áo nam ', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ma_giam_gia`
+--
+
+CREATE TABLE `ma_giam_gia` (
+  `id_ma_giam_gia` int(10) NOT NULL,
+  `ma_giam_gia` varchar(20) DEFAULT NULL,
+  `ngay_tao` date DEFAULT NULL,
+  `Ngay_het_han` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nhan_vien`
+--
+
+CREATE TABLE `nhan_vien` (
+  `ma_nhan_vien` int(11) NOT NULL,
+  `ten_nhan_vien` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `mat_khau` varchar(200) DEFAULT NULL,
+  `avt` varchar(200) DEFAULT NULL,
+  `quyen` tinyint(2) DEFAULT NULL,
+  `trang_thai` tinyint(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -50,6 +173,54 @@ CREATE TABLE `san_pham` (
 --
 
 --
+-- Indexes for table `banner_anh`
+--
+ALTER TABLE `banner_anh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `binh_luan`
+--
+ALTER TABLE `binh_luan`
+  ADD PRIMARY KEY (`ma_binh_luan`);
+
+--
+-- Indexes for table `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  ADD PRIMARY KEY (`ma_chi_tiet_don_hang`);
+
+--
+-- Indexes for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD PRIMARY KEY (`ma_don_hang`);
+
+--
+-- Indexes for table `khach_hang`
+--
+ALTER TABLE `khach_hang`
+  ADD PRIMARY KEY (`ma_khach_hang`);
+
+--
+-- Indexes for table `loai_hang`
+--
+ALTER TABLE `loai_hang`
+  ADD PRIMARY KEY (`ma_loai`);
+
+--
+-- Indexes for table `ma_giam_gia`
+--
+ALTER TABLE `ma_giam_gia`
+  ADD PRIMARY KEY (`id_ma_giam_gia`);
+
+--
+-- Indexes for table `nhan_vien`
+--
+ALTER TABLE `nhan_vien`
+  ADD PRIMARY KEY (`ma_nhan_vien`);
+
+--
 -- Indexes for table `san_pham`
 --
 ALTER TABLE `san_pham`
@@ -58,6 +229,54 @@ ALTER TABLE `san_pham`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `banner_anh`
+--
+ALTER TABLE `banner_anh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `binh_luan`
+--
+ALTER TABLE `binh_luan`
+  MODIFY `ma_binh_luan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  MODIFY `ma_chi_tiet_don_hang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  MODIFY `ma_don_hang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `khach_hang`
+--
+ALTER TABLE `khach_hang`
+  MODIFY `ma_khach_hang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `loai_hang`
+--
+ALTER TABLE `loai_hang`
+  MODIFY `ma_loai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `ma_giam_gia`
+--
+ALTER TABLE `ma_giam_gia`
+  MODIFY `id_ma_giam_gia` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `nhan_vien`
+--
+ALTER TABLE `nhan_vien`
+  MODIFY `ma_nhan_vien` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `san_pham`
